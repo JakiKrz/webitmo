3ec56b85439940207cb37e2d060ed9e613d4b638
JakiKrz <invitt98@gmail.com>
2020-04-16 11:19:07 +0300

    JQuery

---
+++ JQ/jq.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	
+//Работа с селекторами
+	$("p").css("fontSize","20px");
+	$(".anch").css({
+		"fontSize": "30px",
+		"backgroundColor": "#FA8072",
+		"textDecoration" : "none"
+	});
+	$(".paragraph").css("color", "blue");
+
+	$("form *").prop("disabled", true);
+
+
+//Работа с DOM
+
+
+});
